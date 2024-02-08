060fc587222341588af6aee5795d224010521933
shabbat-shalom <jtimesorg@gmail.com>
2024-02-08 13:34:20 -0500

    login

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Script file loaded successfully");
+const wrapper = document.querySelector('.wrapper')
+const registerLink = document.querySelector('.register-link')
+const loginLink = document.querySelector('.login-link')
+const btnPopup = document.querySelector('.btnLogin-popup')
+const iconClose = document.querySelector('.icon-close')
+
+registerLink.onclick = () => {
+    wrapper.classList.add('active');
+};
+
+loginLink.onclick = () => {
+    wrapper.classList.remove('active');
+};
+
+btnPopup.onclick = () => {
+    wrapper.classList.add('active-popup');
+};
+
+iconClose.onclick = () => {
+    wrapper.classList.remove('active-popup');
+    wrapper.classList.remove('active');
+};
